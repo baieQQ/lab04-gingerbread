59908e08d9f90230a32df15eb6de2ed956def917
anniechen201400 <142994746+anniechen201400@users.noreply.github.com>
2026-08-21 01:34:36 +0800

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- story/day_1.py
+++ story/day_1.py
@@ -612,9 +612,9 @@ def draw_end_card(surf):
     veil = pygame.Surface((WINDOW_W, WINDOW_H))
     veil.fill(COL_SILHOUETTE)
     surf.blit(veil, (0, 0))
-    label = end_font.render("第一天　完", True, COL_TITLE)
+    label = end_font.render("白天 完", True, COL_TITLE)
     surf.blit(label, (WINDOW_W // 2 - label.get_width() // 2, WINDOW_H // 2 - 20 * SCALE))
-    sub = hint_font.render("提升自己以迎接晚上", True, COL_BONE)
+    sub = hint_font.render("提升自己 迎接晚上", True, COL_BONE)
     surf.blit(sub, (WINDOW_W // 2 - sub.get_width() // 2, WINDOW_H // 2 + 10 * SCALE))
 
 
